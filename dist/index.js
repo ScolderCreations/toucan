837ab675e11d9cc0ee80d53958192f3609a5abde
ScolderCreations <ScolderCreations@users.noreply.github.com>
2022-02-23 19:55:40 +0000

    Build

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,72 @@
-import ReactDOM from 'react-dom';
-
-function getScreen() {
-  document.getElementById("toucan");
+function getScreen(a) {
+  document.getElementById(a);
 }
 
 var toRender = [];
 var renderObject = "";
 
 class Shape {
-  constructor(width, height, color) {
-    this.width = width || 50;
-    this.height = height || width || 50;
-    this.color = color || "#ffffff";
+  constructor(opt) {
+    this.width = opt.width || 50;
+    this.height = opt.height || opt.width || 50;
+    this.color = opt.color || "#ffffff";
+    this.view = true;
+    this.direction = opt.rotation || 0;
+    this.x = opt.x || 0;
+    this.y = opt.y || 0;
+  }
+
+  show() {
+    this.view = true;
+  }
+
+  hide() {
+    this.view = false;
+  }
+
+  change(whc) {
+    if (whc.width) this.width = whc.width;
+    if (whc.height) this.height = whc.height;
+    if (whc.color) this.color = whc.color;
+  }
+
+}
+
+class Rectangle extends Shape {
+  constructor(opt) {
+    super(opt);
+  }
+
+  render() {
+    return `<rect width=${this.width} height=${this.height} fill=${this.color} ${this.direction ? `transform=${this.direction}` : ''}/>`;
+  }
+
+}
+
+class Circle extends Shape {
+  constructor(opt) {
+    super(opt);
+  }
+
+  render() {
+    return `<ellipse x="${this.x}" rx="${this.width}" ry="${this.height}" fill="${this.color}" ${this.direction ? `transform="${this.direction}"` : ''}/>`;
+  }
+
+}
+
+class Image {
+  constructor(opt) {
+    if (!opt.src.includes('/')) {
+      this.src = opt.src;
+    } else {
+      throw new ReferenceError("Image cannot have no source: please provide URL. \
+      (This error will also show up if you linked a file using \
+        'file.extension', without a ./ or ../)");
+    }
+
+    this.direction = opt.rotation;
+    this.width = opt.width || 50;
+    this.height = opt.height || opt.width || 50;
     this.view = true;
   }
 
@@ -23,83 +78,39 @@ class Shape {
     this.view = false;
   }
 
-  change(width, height, color) {
-    if (width) this.width = width;
-    if (height) this.height = height;
-    if (color) this.color = color;
-  }
-
-}
-
-class Rectangle extends Shape {
-  constructor(width, height, color, rotation) {
-    super(width, height, color);
-    this.direction = rotation;
+  change(whc) {
+    if (whc.width) this.width = whc.width;
+    if (whc.height) this.height = whc.height;
+    if (whc.color) this.color = whc.color;
   }
 
   render() {
-    return /*#__PURE__*/React.createElement("rect", {
-      width: this.width,
-      height: this.height,
-      fill: this.color,
-      transform: this.direction
-    });
+    return `<image width="${this.width}" height="${this.height}" ${this.direction ? `transform="${this.direction}"` : ''} xlink:href="${this.src}"`;
   }
 
 }
 
-class Circle extends Shape {
-  constructor(width, height, color) {
-    super(width, height, color);
-    this.x = 0;
-    this.y = 0;
+class Text {
+  constructor(opt) {
+    this.c = opt.text || String();
+    this.x = opt.x || 0;
+    this.y = opt.y || 0;
   }
 
   render() {
-    return /*#__PURE__*/React.createElement("ellipse", {
-      rx: this.width,
-      ry: this.height,
-      fill: this.color,
-      transform: this.direction
-    });
+    return `<text x="${this.x}" y="10">${this.c}</text>`;
   }
 
 }
 
-class Image {
-  constructor(src, width, height, rotation) {
-    if (!!src) {
-      this.src = src;
-    } else {
-      throw new Error("Image cannot have no source: please provide URL");
-    }
+function drawScreen(a) {
+  renderObject = renderObject + `<svg version="1.1" width=${a.width || 500} height="${a.height || 500}" xmlns="http://www.w3.org/2000/svg">`;
+  toRender.forEach(function (obj) {
+    renderObject = renderObject + obj.render();
+  });
+  renderObject = renderObject + '</svg>';
 
-    this.direction = rotation;
-    this.width = width || 50;
-    this.height = height || width || 50;
-    this.view = true;
-  }
-
-  render() {
-    return React.createElement("image", {
-      width: this.width,
-      height: this.height,
-      transform: this.direction,
-      'xlink:href': this.src
-    });
-  }
-
-}
-
-function drawScreen() {
-  renderObject = renderObject + /*#__PURE__*/React.createElement("svg", {
-    version: "1.1",
-    width: "1440",
-    height: "1080",
-    xmlns: "http://www.w3.org/2000/svg"
-  }, "); toRender.forEach(function(obj) ", renderObject = renderObject + obj.render(), "); renderObject = renderObject + (");
-
-  if (!(getScreen() == renderObject)) {
-    ReactDOM.render(element, document.getElementById('root'));
+  if (!(getScreen(a.src).innerHTML == renderObject)) {
+    getScreen(a.src).innerHTML = renderObject;
   }
 }
