9058c84981ab706356d7dacb334211ec68b5c4d0
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-02-24 08:34:22 -0800

    add @param

--- src/toucan.js
+++ src/toucan.js
@@ -1,17 +1,16 @@
+/* @param a: string */
 function getScreen(a) {
   document.getElementById(a);
 }
+
 function createRender() {
-  renderObject = renderObject + (
-    `<svg version="1.1" width=${a.width || 500} height="${a.height || 500}" xmlns="http://www.w3.org/2000/svg">`
-      );
-  toRender.forEach(function(obj) {
+  renderObject = renderObject + `<svg version="1.1" width=${a.width || 500} height="${a.height || 500}" xmlns="http://www.w3.org/2000/svg">`;
+  toRender.forEach(function (obj) {
     renderObject = renderObject + obj.render();
   });
-  renderObject = renderObject + (
-    '</svg>'
-  );
+  renderObject = renderObject + '</svg>';
 }
+
 var toRender = [];
 var renderObject = "";
 
@@ -22,36 +21,46 @@ class Shape {
     this.color = opt.color || "#ffffff";
     this.view = true;
     this.direction = opt.rotation || 0;
-    this.x = opt.x||0;
-    this.y = opt.y||0;
+    this.x = opt.x || 0;
+    this.y = opt.y || 0;
   }
-  show() {this.view = true}
-  hide() {this.view = false}
+
+  show() {
+    this.view = true;
+  }
+
+  hide() {
+    this.view = false;
+  }
+
   change(whc) {
-    if (whc.width)  this.width = whc.width;
+    if (whc.width) this.width = whc.width;
     if (whc.height) this.height = whc.height;
-    if (whc.color)  this.color = whc.color;
+    if (whc.color) this.color = whc.color;
   }
+
 }
 
 class Rectangle extends Shape {
   constructor(opt) {
     super(opt);
   }
+
   render() {
-    return (
-      `<rect width=${this.width} height=${this.height} fill=${this.color} ${this.direction ? `transform=${this.direction}` : ''}/>`
-    );
+    return `<rect width=${this.width} height=${this.height} fill=${this.color} ${this.direction ? `transform=${this.direction}` : ''}/>`;
   }
+
 }
 
 class Circle extends Shape {
   constructor(opt) {
     super(opt);
   }
+
   render() {
     return `<ellipse x="${this.x}" rx="${this.width}" ry="${this.height}" fill="${this.color}" ${this.direction ? `transform="${this.direction}"` : ''}/>`;
   }
+
 }
 
 class Image {
@@ -59,46 +68,59 @@ class Image {
     if (!opt.src.includes('/')) {
       this.src = opt.src.replace("\"", '\\"');
     } else {
-      throw new ReferenceError ("Image cannot have no source: please provide URL. \
+      throw new ReferenceError("Image cannot have no source: please provide URL. \
       (This error will also show up if you linked a file using \
         'file.extension', without a ./ or ../)");
     }
+
     this.direction = opt.rotation;
     this.width = opt.width || 50;
     this.height = opt.height || opt.width || 50;
-    this.view = true
+    this.view = true;
   }
-  show() {this.view = true}
-  hide() {this.view = false}
+
+  show() {
+    this.view = true;
+  }
+
+  hide() {
+    this.view = false;
+  }
+
   change(whc) {
-    if (whc.width)  this.width = whc.width;
+    if (whc.width) this.width = whc.width;
     if (whc.height) this.height = whc.height;
-    if (whc.color)  this.color = whc.color;
+    if (whc.color) this.color = whc.color;
   }
+
   render() {
     return `<image width="${this.width}" height="${this.height}" ${this.direction ? `transform="${this.direction}"` : ''} xlink:href="${this.src}"`;
   }
+
 }
 
 class Text {
   constructor(opt) {
-    this.c = opt.text.replace('"', '\\"')||String();
-    this.x = opt.x||0;
-    this.y = opt.y||0;
+    this.c = opt.text.replace('"', '\\"') || String();
+    this.x = opt.x || 0;
+    this.y = opt.y || 0;
   }
+
   render() {
     return `<text x="${this.x}" y="10">${this.c}</text>`;
   }
-}
 
-function drawScreen(a) {
+}
+/* @param scr: object */
+function drawScreen(scr) {
   createRender();
-  if (!(getScreen(a.src).innerHTML == renderObject)) {
-    getScreen(a.src).innerHTML = renderObject;
+
+  if (!(getScreen(scr.src).innerHTML == renderObject)) {
+    getScreen(scr.src).innerHTML = renderObject;
   }
 }
 
-function returnScreen(a) {
+function returnScreen(scr) {
   createRender();
   return renderObject;
 }
